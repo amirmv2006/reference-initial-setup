0ff7ac9289ca398577a7ac86053075edb75f76ee
Amir <AmirMV2006@gmail.com>
2020-07-02 19:57:04 +0200

    added code coverage setup

--- src/main/java/ir/amv/snippets/referenceinitialsetup/ReferenceInitialSetupApplication.java
+++ src/main/java/ir/amv/snippets/referenceinitialsetup/ReferenceInitialSetupApplication.java
@@ -1,10 +1,18 @@
 package ir.amv.snippets.referenceinitialsetup;
 
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ReferenceInitialSetupApplication {
+
+  @PostConstruct
+  public void afterRun() {
+    log.info("Application Startup");
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ReferenceInitialSetupApplication.class, args);
